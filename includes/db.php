4928311049bc7c2049fd959cfae89087eb015de6
mahmoudshadid <shadidmahmoud4@gmail.com>
2025-07-16 00:52:19 +0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,18 @@
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASSWORD');
+$host = 'sql8.freesqldatabase.com';
+$db   = 'sql8790234';
+$user = 'sql8790234';
+$pass = 'chg435CWGS';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;port=3306;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    exit('Database connection failed: ' . $e->getMessage());
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
